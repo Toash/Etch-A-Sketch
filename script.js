014a6102d41207af3421f1ad7738f04a5f3f6254
Justin <59674603+Toash@users.noreply.github.com>
2025-01-21 20:46:07 -0500

    added randomize button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#sketchpad")
 const resizeButton = document.querySelector("#resize")
+const randomizeButton = document.querySelector("#randomize")
 
 const chosenColor = document.querySelector("#chosenColor")
 const redButton = document.querySelector("#red")
@@ -11,6 +12,14 @@ const violetButton = document.querySelector("#violet")
 const whiteButton = document.querySelector("#white")
 const blackButton = document.querySelector("#black")
 
+function getRandomColor() {
+    let letters = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
 
 function changeColor(color) {
     redButton.classList.remove("colorSelected")
@@ -21,6 +30,8 @@ function changeColor(color) {
     violetButton.classList.remove("colorSelected")
     whiteButton.classList.remove("colorSelected")
     blackButton.classList.remove("colorSelected")
+
+    randomizeButton.classList.remove("randomizeSelected")
 
     switch (color) {
         case "red":
@@ -47,6 +58,9 @@ function changeColor(color) {
         case "black":
             blackButton.classList.add("colorSelected")
             break;
+        case "randomize":
+            randomizeButton.classList.add("randomizeSelected")
+            break;
     }
     currentColor = color;
     console.log("Changed color")
@@ -60,6 +74,7 @@ blueButton.addEventListener("click", () => changeColor("blue"))
 violetButton.addEventListener("click", () => changeColor("violet"))
 whiteButton.addEventListener("click", () => changeColor("white"))
 blackButton.addEventListener("click", () => changeColor("black"))
+randomizeButton.addEventListener("click", () => changeColor("randomize"))
 
 changeColor("red")
 
@@ -94,7 +109,6 @@ function deleteGrid() {
     }
 }
 
-
 let isDrawing = false;
 document.addEventListener("mousedown", (e) => {
     e.preventDefault() // for some reason this makes drawing much smoother
@@ -110,8 +124,13 @@ document.addEventListener("mouseup", (e) => {
 function drawGrid(tilesPerRow) {
 
     function colorTile(div, color) {
-        //div.setAttribute("style", `background-color: ${color}`)
-        div.style.backgroundColor = color;
+        if (color === "randomize") {
+            div.style.backgroundColor = getRandomColor();
+        } else {
+            div.style.backgroundColor = color;
+        }
+
+
     }
 
     let tileWidth = containerWidth / tilesPerRow
